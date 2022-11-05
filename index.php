a0c1536dfee46cc909007b50da8a0fecca2e1fea
MD Mazbaul Islam <mazbaul20@gmail.com>
2022-11-05 15:27:48 +0600

    update data

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 ?>
 <h2>Array Function</h2>
 <?php
-    include 'classes/array_diff_key.php';
+    $name = array("Mazbaul","Atikur","Monnaf","Jibon","Mijanur","Siddikul");
+    $age  = array(21,23,55,29,16,32);
+
+    $combine = array_combine($name,$age);
+    print("<pre>");
+    print_r($combine);
+    print("</pre>");
 ?>
 
 
